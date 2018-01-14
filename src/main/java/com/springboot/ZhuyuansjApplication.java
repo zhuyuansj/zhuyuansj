5e443de23ef76bd7b1c62b110288e0469a8c0a0c
shimilysj <124954495@qq.com>
2018-01-14 14:22:38 +0800

    增加generator功能

--- src/main/java/com/springboot/ZhuyuansjApplication.java
+++ src/main/java/com/springboot/ZhuyuansjApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@MapperScan("com.springboot.mapper")//将项目中对应的mapper类的路径加进来就可以了
 public class ZhuyuansjApplication {
 
 	public static void main(String[] args) {
